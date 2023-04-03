904cd7ed150610b931505dd714b7c6e204b0d1eb
Aleksandra Mochol <mocholaleksandra@gmail.com>
2023-04-03 19:56:20 +0200

    Game of Life - 03.04 update

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from board import init, display
+from config import Settings
 
-WIDTH = 10
-HEIGHT = 10
+settings=Settings()
 
-board = init(WIDTH,HEIGHT)
+board = init(settings.width, settings.height)
 
 board[4][5] = 1
 board[4][6] = 1
 board[5][5] = 1
 
 display(board)
-
-'''for x in range(HEIGHT):
-    row = []
-    for y in range(WIDTH):
-        row.append(0)
-    board.append(row)'''
